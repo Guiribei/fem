2a4e36e26cd5b266a6f8a3bd7b3f2ae2b3621a4f
Guiribei <guilherme_f_ribeiro@hotmail.com>
2025-04-16 23:36:47 -0300

    basic server

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
 func main() {
+	http.HandleFunc("/", 
+	func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hellow from Go program"))
+	})
 	
+	err := http.ListenAndServe(":8000", nil)
+	if err == nil{
+		fmt.Println("Error while opening the server")
+		os.Exit(1)
+	}
 }
