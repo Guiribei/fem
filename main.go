1beeaa780c4e6e882f07f2348a5b4af71d93e52d
Guiribei <guilherme_f_ribeiro@hotmail.com>
2025-04-16 23:57:24 -0300

    add static files

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import (
 )
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hellow from Go program!"))
+	w.Write([]byte("Hello from Go program!"))
 }
 
 func main() {
 	server := http.NewServeMux()
-	server.HandleFunc("/", handleRoot)
+	
+	server.HandleFunc("/hello", handleRoot)
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
 
 	err := http.ListenAndServe(":8000", server)
-	if err == nil{
-		fmt.Println("Error while opening the server")
+	if err != nil {
+		fmt.Println("Error while opening the server:", err)
 		os.Exit(1)
 	}
 }
